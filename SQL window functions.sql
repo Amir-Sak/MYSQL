22980f997452580834f065c8796e8f4c2c50e7d7
Amir Saker <iamsakeramir@gmail.com>
2024-05-19 22:29:38 +0200

    SQL window functions
    SQL window functions are powerful for performing calculations across a set of table rows related to the current row.

---
+++ SQL window functions.sql
@@ -0,0 +1,28 @@
+-- SQL window functions are powerful for performing calculations across a set of table rows related to the current row. Let's go through some common SQL window functions with examples:
+
+-- 1. ROW_NUMBER()
+-- The ROW_NUMBER() function assigns a unique sequential integer to rows within a partition of a result set, starting at 1 for the first row in each partition
+
+	SELECT 
+    company, 
+    total_laid_off, 
+    ROW_NUMBER() OVER (ORDER BY total_laid_off DESC) AS row_num
+FROM 
+    layoffs_test2;
+-- 2. RANK()
+-- The RANK() function assigns a rank to each row within a partition of a result set. Ranks may have gaps: the next rank after a tie is the rank plus the number of tied rows.
+
+SELECT 
+    company, 
+    total_laid_off, 
+    RANK() OVER (ORDER BY total_laid_off DESC) AS rankd
+FROM 
+    layoffs_test2;
+-- 3. DENSE_RANK()
+-- The DENSE_RANK() function assigns ranks to rows within a partition of a result set, similar to RANK(), but without gaps between rank values.
+SELECT 
+    company, 
+    total_laid_off, 
+    DENSE_RANK() OVER (ORDER BY total_laid_off DESC) AS dense_ryank
+FROM 
+    layoffs_test2;
